cc13d871d26e367fa8b8fbc9f837e960b5d33740
Ahmad Zuhal Zhafran <90975889+azuhalz@users.noreply.github.com>
2026-04-22 05:56:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Counter</h1>
+      <span id="value">{{ count }}</span>
+      <div>
+        <button @click="count--" id="decrease">-</button>
+        <button @click="count++" id="increase">+</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+  background-color: gray;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+#value {
+  font-size: 50px;
+}
+
+button {
+  margin: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  background-color: #555555;
+}
+
+button:hover {
+  background-color: #777777;
+}
+</style>
